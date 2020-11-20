fde80e7f11551cfd8fcbc991d654c7b48ec7bf92
Nikolay Mahotkin <nikolay.makhotkin@gmail.com>
2020-11-20 23:32:28 +0400

    Fix promts

--- gptrick.py
+++ gptrick.py
@@ -553,7 +553,7 @@ def main():
         # Let's chat for 5 lines
         for step in range(5):
             # encode the new user input, add the eos_token and return a tensor in Pytorch
-            new_user_input_ids = tokenizer.encode(input(">> User:") + tokenizer.eos_token, return_tensors='pt')
+            new_user_input_ids = tokenizer.encode(input(">> User: ") + tokenizer.eos_token, return_tensors='pt')
             # print(new_user_input_ids)
 
             # append the new user input tokens to the chat history
@@ -571,7 +571,7 @@ def main():
             )
 
             # pretty print last ouput tokens from bot
-            print("RickBot: {}".format(
+            print("Bot: {}".format(
                 tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)))
 
     return results
